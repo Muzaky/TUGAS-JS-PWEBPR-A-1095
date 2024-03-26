d3dcb585cbaae1648844e94c0eca75aca5a24408
Muzaky <afif050503@gmail.com>
2024-03-26 22:09:18 +0700

    FIXEDDDDDDDDDDDDD

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ document.getElementById('yes').addEventListener('click', function(){
     var img = document.getElementById('detektif');
     img.src = './assets/pembohong.png';
     document.getElementById('text').innerText = 'Anda Berbohong!';
-    document.querySelector('.container > .main-container').style.backgroundColor = '#E12E2E';
-    document.querySelector('.container > .main-container > p').innerText = 'Anda adalah seorang Teknisi'
+    document.querySelector('.main > .container').style.backgroundColor = '#E12E2E';
+    document.querySelector('.main > .container > p').innerText = 'Anda adalah seorang Teknisi'
 });
 
-document.getElementById('tidak').addEventListener('click', function(){
+document.getElementById('no').addEventListener('click', function(){
     buttons.forEach(function(button) {
         button.remove();
     });
     var img = document.getElementById('detektif');
-    img.src = './resources/pembernar.png';
+    img.src = './assets/pembenar.png';
     document.getElementById('text').innerText = 'Anda Benar!';
-    document.querySelector('.container > .main-container').style.backgroundColor = '#6FD240';
-    document.querySelector('.container > .main-container > p').innerText = 'Anda adalah seorang Mahasiswa'
+    document.querySelector('.main > .container').style.backgroundColor = '#6FD240';
+    document.querySelector('.main > .container > p').innerText = 'Anda adalah seorang Mahasiswa'
 });
